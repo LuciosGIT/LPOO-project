34acd9cf77e29e916d6117cec3a54d46ce9f1e87
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-10 19:17:14 -0300

    refactor : changes enum TipoArma signature to support damage

--- src/main/java/org/example/enums/TipoArma.java
+++ src/main/java/org/example/enums/TipoArma.java
@@ -2,16 +2,19 @@ package org.example.enums;
 
 public enum TipoArma {
 
-    CORPO(0, "corpo a corpo"),
-    DISTANCIA(10, "à distância");
+    CORPO(0, "corpo a corpo", 10.0),
+    DISTANCIA(10, "à distância", 20.0);
 
     private Integer municao;
 
     private String tipoAtaque;
 
-    TipoArma(Integer municao, String tipoAtaque) {
+    private Double dano;
+
+    TipoArma(Integer municao, String tipoAtaque, Double dano) {
         this.municao = municao;
         this.tipoAtaque = tipoAtaque;
+        this.dano = dano;
     }
 
     public String getValue() {
@@ -24,5 +27,9 @@ public enum TipoArma {
 
     public void setMunicao(Integer novaMunicao) {
         this.municao = novaMunicao;
+    }
+
+    public Double getDano() {
+        return this.dano;
     }
 }
