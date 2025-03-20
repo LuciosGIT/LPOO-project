735769e4049fd879f14da652608ad10805647da8
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-20 19:45:47 -0300

    feat : creates Evento interface

--- src/main/java/org/example/interfaces/EventoInterface.java
+++ src/main/java/org/example/interfaces/EventoInterface.java
@@ -1,2 +1,9 @@
-package org.example.interfaces;public interface EventoInterface {
+package org.example.interfaces;
+
+import org.example.domain.Ambiente;
+import org.example.domain.Personagem;
+
+public interface EventoInterface {
+
+    public void executar(Personagem jogador, Ambiente local);
 }
