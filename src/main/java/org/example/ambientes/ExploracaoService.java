1eba6da70616b11c1bf3e5ffb183fede1b13277b
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-03-25 15:32:13 -0300

    feat : updates ExploracaoService with EventoCriatura implementation

--- src/main/java/org/example/ambientes/ExploracaoService.java
+++ src/main/java/org/example/ambientes/ExploracaoService.java
@@ -1,52 +1,65 @@
 package org.example.ambientes;
 
+import org.example.domain.Evento;
 import org.example.domain.Item;
 import org.example.domain.Personagem;
+import org.example.eventos.EventoCriatura;
+import org.example.utilitarios.Utilitario;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class ExploracaoService {
 
-        public static void explorar(Personagem jogador, List<Item> recursosDisponiveis, Double dificuldadeExploracao) {
+        public static void explorar(Personagem jogador, List<Item> recursosDisponiveis, List<Evento> eventos, Double dificuldadeExploracao) {
             if (dificuldadeExploracao < 5 && jogador.getInventario().temEspaco()) {
                 // Encontrou monstro? Se não, passa a procurar itens
-                if (getValorAleatorio() < dificuldadeExploracao) {
-                    System.out.println("Você encontrou um monstro! Prepare-se para a batalha!");
-                    return; // Encerrar a exploração em caso de batalha
-                }
+                if (Utilitario.getValorAleatorio() < dificuldadeExploracao) {
 
-                if (jogador.getVida() > 0) {
-                    boolean encontrouItem = false;
+                    // Filtra apenas os eventos de criatura
+                    List<EventoCriatura> eventosCriatura = eventos.stream()
+                            .filter(evento -> evento instanceof EventoCriatura)
+                            .map(evento -> (EventoCriatura) evento)
+                            .collect(Collectors.toList());
 
-                    for (Item recurso : recursosDisponiveis) {
-                        if (getValorAleatorio() < recurso.getProbabilidadeDeEncontrar()) {
-                            if (recurso.getNomeItem().equals("Cogumelo") && getValorAleatorio() < 0.2) {
-                                System.out.println("Você coletou um cogumelo, porém ele está envenenado!");
-                                jogador.diminuirVida(15.0);
-                                jogador.diminuirSanidade(5.0);
-                                jogador.diminuirEnergia(15.0);
-                                encontrouItem = true;
-                                break;
-                            }
-
-                            recurso.alterarPersonagem(jogador);
-                            jogador.getInventario().adicionarItem(recurso);
-                            System.out.printf("Você coletou um(a) %s%n", recurso.getNomeItem());
-                            encontrouItem = true;
+                    // Verifica se ocorre um evento de criatura
+                    for (EventoCriatura evento : eventosCriatura) {
+                        if (Utilitario.getValorAleatorio() < evento.getProbabilidadeOcorrencia()) {
+                            System.out.printf("Você encontrou um(a) %s! Prepare-se para a batalha!%n", evento.getCriatura().getNome());
+                            evento.getCriatura().ataque(jogador);
+                            return; // Encerra a exploração se encontrou uma criatura
                         }
                     }
 
-                    if (!encontrouItem) {
-                        System.out.println("Nenhum item encontrado.");
+                    if (jogador.getVida() > 0) {
+                        boolean encontrouItem = false;
+
+                        for (Item recurso : recursosDisponiveis) {
+                            if (Utilitario.getValorAleatorio() < recurso.getProbabilidadeDeEncontrar()) {
+                                if (recurso.getNomeItem().equals("Cogumelo") && Utilitario.getValorAleatorio() < 0.2) {
+                                    System.out.println("Você coletou um cogumelo, porém ele está envenenado!");
+                                    jogador.diminuirVida(15.0);
+                                    jogador.diminuirSanidade(5.0);
+                                    jogador.diminuirEnergia(15.0);
+                                    encontrouItem = true;
+                                    break;
+                                }
+
+                                recurso.alterarPersonagem(jogador);
+                                jogador.getInventario().adicionarItem(recurso);
+                                System.out.printf("Você coletou um(a) %s%n", recurso.getNomeItem());
+                                encontrouItem = true;
+                            }
+                        }
+
+                        if (!encontrouItem) {
+                            System.out.println("Nenhum item encontrado.");
+                        }
                     }
                 }
             }
-        }
 
-        private static double getValorAleatorio() {
-            Random random = new Random();
-            return random.nextDouble();
         }
     }
 
