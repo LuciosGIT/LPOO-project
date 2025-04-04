3eaee11d7088cdfdf2719aeb6b1d4d4fcb232aba
EmmanoelB03 <emmanoel.barbosa03@gmail.com>
2025-04-04 15:00:12 -0300

    feat: Switch MyGame to extend Game and set an initial screen
    Refactored MyGame to inherit from Game instead of ApplicationAdapter, enabling screen management. Initialized the game by setting TelaDeInicio as the starting screen.

--- src/main/java/org/example/game/MyGame.java
+++ src/main/java/org/example/game/MyGame.java
@@ -1,21 +1,19 @@
 package org.example.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import org.example.telasDoJogo.TelaDeInicio;
 
-public class MyGame extends ApplicationAdapter {
+public class MyGame extends Game {
 
     @Override
     public void create() {
         // Inicialização do seu jogo
-    }
 
-    @Override
-    public void render() {
-        // Lógica do jogo, chamada a cada quadro
-        GL20 gl = com.badlogic.gdx.Gdx.gl;
-        gl.glClearColor(0, 0, 0, 1);  // Cor de fundo
-        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);  // Limpa a tela
+        setScreen( new TelaDeInicio());
+
     }
 
     @Override
