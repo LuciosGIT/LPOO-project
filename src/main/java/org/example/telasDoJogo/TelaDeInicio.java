0fd201cd6a18a5aeac64b67063dcace6d89762e1
EmmanoelB03 <emmanoel.barbosa03@gmail.com>
2025-04-05 11:59:52 -0300

    refactor: refactor TelaDeInicio to modularize stage and button setup
    Extracted stage and button initialization logic into separate methods (`inicializarElementos` and `criarBotao`) for better readability and maintainability. Added a new "Sair" button with functionality to exit the application.

--- src/main/java/org/example/telasDoJogo/TelaDeInicio.java
+++ src/main/java/org/example/telasDoJogo/TelaDeInicio.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -12,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 
 public class TelaDeInicio implements Screen {
@@ -19,35 +21,19 @@ public class TelaDeInicio implements Screen {
     private Stage stage;
     private SpriteBatch batch;
     private Texture backgroundTexture;
-    private Texture buttonTexture;
+    private Texture buttonTextureJogar;
+    private Texture buttonTextureSair;
+
+
 
     @Override
     public void show() {
-        // Inicializar o viewport e stage
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
 
-        // Inicializar o SpriteBatch e a textura como campos da classe
-        batch = new SpriteBatch();
-        backgroundTexture = new Texture("backgroundTelaDeInicio.png");
+        inicializarElementos();
 
-        //centralizar
+        criarBotao();
 
 
-
-        //criar botões
-        buttonTexture = new Texture("imagemBotaoJogar.png");
-        ImageButton buttonJogar = new ImageButton(new TextureRegionDrawable(new TextureRegion(buttonTexture)));
-
-        buttonJogar.setSize(buttonJogar.getWidth()*0.5f, buttonJogar.getHeight()*0.2f);
-        buttonJogar.setPosition((float) Gdx.graphics.getWidth() /2-buttonJogar.getWidth()/2, (float) Gdx.graphics.getHeight() /2-buttonJogar.getHeight()/2);
-
-        buttonJogar.addListener(new ClickListener(){
-
-
-        });
-
-        stage.addActor(buttonJogar);
 
     }
 
@@ -77,12 +63,12 @@ public class TelaDeInicio implements Screen {
     @Override
     public void dispose() {
         // Liberar recursos
-        buttonTexture.dispose();
+        buttonTextureJogar.dispose();
+        buttonTextureJogar.dispose();
         stage.dispose();
         batch.dispose();
         backgroundTexture.dispose();
     }
-
 
     @Override
     public void render(float delta) {
@@ -99,6 +85,53 @@ public class TelaDeInicio implements Screen {
         // Atualizar e desenhar o stage
         stage.act(delta);
         stage.draw();
+    }
+
+    private void inicializarElementos(){
+
+        // Inicializar o viewport e stage
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        // Inicializar o SpriteBatch e a textura como campos da classe
+        batch = new SpriteBatch();
+        backgroundTexture = new Texture("backgroundTelaDeInicio.png");
+
+    }
+
+    private void criarBotao(){
+
+        buttonTextureJogar = new Texture("imagemBotaoJogar.png");
+        buttonTextureSair = new Texture("imagemBotaoSair.png");
+
+        ImageButton buttonJogar = new ImageButton(new TextureRegionDrawable(new TextureRegion(buttonTextureJogar)));
+        ImageButton buttonSair = new ImageButton(new TextureRegionDrawable(new TextureRegion(buttonTextureSair)));
+
+        buttonJogar.setSize(buttonJogar.getWidth()*0.5f, buttonJogar.getHeight()*0.2f);
+        buttonJogar.setPosition((float) Gdx.graphics.getWidth() /2-buttonJogar.getWidth()/2, (float) Gdx.graphics.getHeight() /2-buttonJogar.getHeight()/2);
+
+        buttonSair.setSize(buttonSair.getWidth()*0.3f, buttonSair.getHeight()*0.15f);
+        buttonSair.setPosition((float) Gdx.graphics.getWidth() /2-buttonSair.getWidth()/2, (float) Gdx.graphics.getHeight() /2.8f-buttonSair.getHeight()/2);
+
+        buttonJogar.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("oi");
+                super.clicked(event, x, y);
+            }
+        });
+
+        buttonSair.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+
+        stage.addActor(buttonSair);
+        stage.addActor(buttonJogar);
+
     }
 
 }
