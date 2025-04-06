862929eada0aa2c5be98f256bac5ef37f9ecdd1f
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-04-06 17:33:34 -0300

    remove : excludes `dano` property from `TipoArma` enum
    Eliminated the `dano` attribute and its related methods from `TipoArma`. This simplifies the enum by focusing only on `municao` and `tipoAtaque` while removing unused or redundant functionality.

--- src/main/java/org/example/enums/TipoArma.java
+++ src/main/java/org/example/enums/TipoArma.java
@@ -2,19 +2,16 @@ package org.example.enums;
 
 public enum TipoArma {
 
-    CORPO(0, "corpo a corpo", 10.0),
-    DISTANCIA(10, "à distância", 20.0);
+    CORPO(0, "corpo a corpo"),
+    DISTANCIA(10, "à distância");
 
     private Integer municao;
 
     private String tipoAtaque;
 
-    private Double dano;
-
-    TipoArma(Integer municao, String tipoAtaque, Double dano) {
+    TipoArma(Integer municao, String tipoAtaque) {
         this.municao = municao;
         this.tipoAtaque = tipoAtaque;
-        this.dano = dano;
     }
 
     public String getValue() {
@@ -34,7 +31,4 @@ public enum TipoArma {
         this.municao += municao;
     }
 
-    public Double getDano() {
-        return this.dano;
-    }
 }
