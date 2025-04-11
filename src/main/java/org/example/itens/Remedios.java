2e73db8c4ee0d655fa46ea5a49cb6397388185a6
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-04-11 19:16:25 -0300

    feat: refactor Remedios class to derive tipoEfeito from TipoRemedio and adjust healing logic

--- src/main/java/org/example/itens/Remedios.java
+++ src/main/java/org/example/itens/Remedios.java
@@ -11,16 +11,16 @@ public class Remedios extends Item {
 
     private TipoEfeito tipoEfeito;
 
-    public Remedios(String nomeItem, Personagem personagem, Double peso, Double durabilidade, Double probabilidadeDeEncontrar, TipoRemedio tipoRemedio, TipoEfeito tipoEfeito) {
+    public Remedios(String nomeItem, Personagem personagem, Double peso, Double durabilidade, Double probabilidadeDeEncontrar, TipoRemedio tipoRemedio) {
         super( nomeItem, personagem, peso, durabilidade, probabilidadeDeEncontrar);
         this.tipoRemedio = tipoRemedio;
-        this.tipoEfeito = tipoEfeito;
+        this.tipoEfeito = tipoRemedio.getValue();
     }
 
     @Override
     public void usar() {
         switch (tipoEfeito) {
-            case CURAR -> this.getPersonagem().aumentarVida(15.0);
+            case CURAR -> this.getPersonagem().aumentarVida(100.0 - this.getPersonagem().getVida());
             case ALIVIAR -> this.getPersonagem().aumentarVida(5.0);
             case TRATAR -> this.getPersonagem().aumentarVida(10.0);
         }
