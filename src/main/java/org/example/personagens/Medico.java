81fee56fad345f93a795533605e2b9920d7c0b2b
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-04-11 19:17:44 -0300

    feat: add healing functionality to Medico class and initialize inventory with medicines

--- src/main/java/org/example/personagens/Medico.java
+++ src/main/java/org/example/personagens/Medico.java
@@ -2,14 +2,42 @@ package org.example.personagens;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import org.example.domain.Personagem;
+import org.example.enums.TipoEfeito;
+import org.example.enums.TipoFerramenta;
+import org.example.enums.TipoMaterial;
+import org.example.enums.TipoRemedio;
+import org.example.itens.Ferramentas;
+import org.example.itens.Materiais;
+import org.example.itens.Remedios;
 
 import java.util.HashMap;
 
 public class Medico extends Personagem {
 
+    private Double cura;
+
     private HashMap<String, TextureRegion> sprites;
 
     public Medico(String nome) {
+        this.alterarNomePersonagem(nome);
+        this.cura = 15.0;
+
+        this.getInventario().adicionarItem(new Remedios("Bandagem Auxiliar",
+                this,
+                7.0,
+                90.0,
+                0.4,
+                TipoRemedio.BANDAGEM
+               ));
+
+        this.getInventario().adicionarItem(new Remedios("Soro Antibiótico",
+                this,
+                7.0,
+                90.0,
+                0.4,
+                TipoRemedio.ANTIBIOTICO
+        ));
+
         this.alterarNomePersonagem(nome);
         //to do: criar o hashmap passando os endereçoes das texturas
     }
@@ -21,6 +49,10 @@ public class Medico extends Personagem {
 
     @Override
     public void habilidade() {
-    // to do
+        tratarFerimentos(this);
+    }
+
+    public void tratarFerimentos(Personagem personagem) {
+        personagem.aumentarVida(this.cura);
     }
 }
