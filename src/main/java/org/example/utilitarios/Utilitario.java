fcbfee92dbc917d59e7c22f4d2df2283b7cbeab3
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-04-12 11:01:29 -0300

    feat: add spawn and trap mechanics for Morcego and Sobrevivente in Utilitario class

--- src/main/java/org/example/utilitarios/Utilitario.java
+++ src/main/java/org/example/utilitarios/Utilitario.java
@@ -1,5 +1,10 @@
 package org.example.utilitarios;
 
+import org.example.criatura.Morcego;
+import org.example.criatura.Sobrevivente;
+import org.example.domain.Criatura;
+import org.example.domain.Personagem;
+
 import java.util.Random;
 
 public class Utilitario {
@@ -12,5 +17,38 @@ public class Utilitario {
     public static boolean getBooleanAleatorio(){
         Random random = new Random();
         return random.nextBoolean();
+    }
+
+    public static boolean spawnarMorcego(Personagem jogador) {
+        boolean foiAtacado = false;
+        foiAtacado = Utilitario.getBooleanAleatorio();
+        if (foiAtacado) {
+            Criatura criatura = new Morcego("Morcego Espantoso", 100.0, 25.0, 25.0);
+            criatura.ataque(jogador);
+        }
+            return foiAtacado;
+
+    }
+
+    public static boolean spawnarSobrevivente(Personagem jogador) {
+        boolean foiAtacado = false;
+        foiAtacado = Utilitario.getBooleanAleatorio();
+        if (foiAtacado) {
+            Criatura criatura = new Sobrevivente("Sobrevivente Perdido", 100.0, 15.0, 15.0);
+            criatura.ataque(jogador);
+        }
+        return foiAtacado;
+
+    }
+
+    public static boolean armadilhaInstaurada(Personagem jogador) {
+        boolean armadilhaInstaurada = false;
+        armadilhaInstaurada = Utilitario.getBooleanAleatorio();
+        if (armadilhaInstaurada) {
+            jogador.diminuirVida(25.0);
+            jogador.diminuirEnergia(15.0);
+            jogador.diminuirSanidade(22.0);
+        }
+        return armadilhaInstaurada;
     }
 }
