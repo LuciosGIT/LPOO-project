de550676049591bd916866d49424d555e9630c3f
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-04-25 15:22:59 -0300

    feat: enhance AmbienteMontanha with unstable climate and new resource events

--- src/main/java/org/example/ambientes/AmbienteMontanha.java
+++ src/main/java/org/example/ambientes/AmbienteMontanha.java
@@ -4,12 +4,17 @@ import org.example.domain.Ambiente;
 import org.example.domain.Evento;
 import org.example.domain.Item;
 import org.example.domain.Personagem;
+import org.example.enums.Pureza;
 import org.example.enums.TipoAlimento;
 import org.example.enums.TipoClimatico;
 import org.example.enums.TipoMaterial;
+import org.example.eventos.EventoClimatico;
+import org.example.eventos.EventoCriatura;
 import org.example.gerenciadores.GerenciadorDeEventos;
+import org.example.itens.Agua;
 import org.example.itens.Alimentos;
 import org.example.itens.Materiais;
+import org.example.utilitarios.Utilitario;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -17,16 +22,27 @@ import java.util.Random;
 
 public class AmbienteMontanha extends Ambiente {
 
-    Boolean terrenoAcidentado;
+    private boolean terrenoAcidentado; // exige mais energia para exploração
+
+    private boolean climaInstavel; // nevasca e vento ocorrem repentinamente
+
 
     //construtor
-    public AmbienteMontanha(String nome, String descricao, Double dificuldadeExploracao, List<TipoClimatico> condicoesClimaticas, Boolean terrenoAcidentado){
+    public AmbienteMontanha(String nome, String descricao, Double dificuldadeExploracao, List<TipoClimatico> condicoesClimaticas, boolean terrenoAcidentado, boolean climaInstavel){
         super(nome, descricao, dificuldadeExploracao, condicoesClimaticas);
         this.getRecursosDisponiveis().add(new Alimentos("Carne", null, 1.0, 8.0, 0.3, TipoAlimento.CARNE, OffsetDateTime.now().plusDays(10)));
         this.getRecursosDisponiveis().add(new Alimentos("Raíz", null, 0.3, 12.0, 0.5, TipoAlimento.RAIZES, OffsetDateTime.now().plusDays(12)));
-        this.getRecursosDisponiveis().add(new Materiais("Pedra", null, 8.0, 20.0, 0.7, 10.0, TipoMaterial.PEDRA));
+        this.getRecursosDisponiveis().add(new Materiais("Pedra Cristalina", null, 8.0, 20.0, 0.7, 10.0, TipoMaterial.PEDRA));
         this.getRecursosDisponiveis().add(new Materiais("Madeira", null, 2.0, 20.0, 0.5, 5.0, TipoMaterial.MADEIRA));
+        this.getRecursosDisponiveis().add(new Agua("Água de Degelo", null, 5.0, 20.0, 0.5, Pureza.CONTAMINADA ,10.0));
+        this.getEventos().add(new EventoCriatura(true, "Batalha", "Evento de Criatura", 0.7,
+                getCriaturasAmbientes().get(3) , getCriaturasAmbientes().get(1).getNivelDePerigo()));
+        this.getEventos().add(new EventoCriatura(true, "Batalha", "Evento de Criatura", 0.5,
+                getCriaturasAmbientes().get(0), getCriaturasAmbientes().get(3).getNivelDePerigo()));
+        this.getEventos().add(new EventoClimatico(true, "Evento de Nevasca foi acionado", "Afeta a visibilidade", "Evento de Nevasca" , 0.5, TipoClimatico.NEVASCA, 5, "A nevasca reduz a visibilidade e a temperatura, dificultando a exploração e aumentando o consumo de energia."));
         this.terrenoAcidentado = terrenoAcidentado;
+        this.climaInstavel = climaInstavel;
+
     }
 
     public AmbienteMontanha() { }
@@ -61,6 +77,10 @@ public class AmbienteMontanha extends Ambiente {
             jogador.diminuirEnergia(4.0*getDificuldadeExploracao());
             jogador.diminuirSede(1.0*getDificuldadeExploracao());
             jogador.diminuirFome(1.0*getDificuldadeExploracao());
+        }
+
+        if(this.climaInstavel) {
+            this.getEventos().get(1).setProbabilidadeOcorrencia(this.getEventos().get(1).getProbabilidadeOcorrencia() + 0.2);
         }
 
     }
