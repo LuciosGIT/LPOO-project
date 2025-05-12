2b66fc5593f204144409ce2808d30f06f6934cd4
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-05-12 15:52:34 -0300

    feat: implement weighted random event selection in GerenciadorDeEventos

--- src/main/java/org/example/criatura/Crocodilo.java
+++ src/main/java/org/example/criatura/Crocodilo.java
@@ -1,4 +1,15 @@
 package org.example.criatura;
 
-public class Crocodilo {
+import org.example.domain.Criatura;
+import org.example.domain.Personagem;
+
+public class Crocodilo extends Criatura {
+
+    public Crocodilo(String nome, Double vida, Double nivelDePerigo, Double danoDeAtaque) {
+        super(nome, vida, nivelDePerigo, danoDeAtaque);
+    }
+    @Override
+    public void ataque(Personagem jogador) {
+
+    }
 }
