d3811fce07cb161987a5d5c727f5fa75d270c028
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-05-22 13:32:56 -0300

    feat: change gerarEvento method to return Evento object instead of void

--- src/main/java/org/example/interfaces/AmbienteInterface.java
+++ src/main/java/org/example/interfaces/AmbienteInterface.java
@@ -1,12 +1,13 @@
 package org.example.interfaces;
 
+import org.example.domain.Evento;
 import org.example.domain.Personagem;
 
 public interface AmbienteInterface {
 
     public void explorar(Personagem jogador);
 
-    public void gerarEvento(Personagem jogador);
+    public Evento gerarEvento(Personagem jogador);
 
     public void modificarClima();
 
