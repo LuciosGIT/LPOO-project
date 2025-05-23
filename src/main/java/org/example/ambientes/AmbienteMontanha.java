3ac7cc6f887d3c95eb2d01b1b000292a50699b5d
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-05-23 14:12:27 -0300

    feat: add discovery event for cave with resource rewards

--- src/main/java/org/example/ambientes/AmbienteMontanha.java
+++ src/main/java/org/example/ambientes/AmbienteMontanha.java
@@ -4,12 +4,10 @@ import org.example.domain.Ambiente;
 import org.example.domain.Evento;
 import org.example.domain.Item;
 import org.example.domain.Personagem;
-import org.example.enums.Pureza;
-import org.example.enums.TipoAlimento;
-import org.example.enums.TipoClimatico;
-import org.example.enums.TipoMaterial;
+import org.example.enums.*;
 import org.example.eventos.EventoClimatico;
 import org.example.eventos.EventoCriatura;
+import org.example.eventos.EventoDescoberta;
 import org.example.gerenciadores.GerenciadorDeEventos;
 import org.example.itens.Agua;
 import org.example.itens.Alimentos;
@@ -39,6 +37,8 @@ public class AmbienteMontanha extends Ambiente {
                 getCriaturasAmbientes().get(3) , getCriaturasAmbientes().get(1).getNivelDePerigo()));
         this.getEventos().add(new EventoCriatura(true, "Batalha", "Evento de Criatura", 0.5,
                 getCriaturasAmbientes().get(0), getCriaturasAmbientes().get(3).getNivelDePerigo()));
+        this.getEventos().add(new EventoDescoberta(true, "Descoberta", "Evento de Descoberta", 0.7,"Você pode encontrar Morcego",
+                TipoDescoberta.CAVERNA, List.of(new Materiais("Pedra", player, 2.0, 20.0, 0.8, 5.0, TipoMaterial.PEDRA))));
         this.getEventos().add(new EventoClimatico(true, "Evento de Nevasca foi acionado", "Afeta a visibilidade", "Evento de Nevasca" , 0.7, TipoClimatico.NEVASCA, 5, "A nevasca reduz a visibilidade e a temperatura, dificultando a exploração e aumentando o consumo de energia."));
         this.terrenoAcidentado = terrenoAcidentado;
         this.climaInstavel = climaInstavel;
