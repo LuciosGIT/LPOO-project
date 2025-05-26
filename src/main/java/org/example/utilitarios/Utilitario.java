05d0fbefcba8cadd46efb8df356e1ea62139cc8c
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-05-26 00:19:56 -0300

    remove: eliminate unused spawn and trap methods from Utilitario class

--- src/main/java/org/example/utilitarios/Utilitario.java
+++ src/main/java/org/example/utilitarios/Utilitario.java
@@ -19,36 +19,4 @@ public class Utilitario {
         return random.nextBoolean();
     }
 
-    public static boolean spawnarMorcego(Personagem jogador) {
-        boolean foiAtacado = false;
-        foiAtacado = Utilitario.getBooleanAleatorio();
-        if (foiAtacado) {
-            Criatura criatura = new Morcego("Morcego Espantoso", 100.0, 25.0, 25.0);
-            criatura.ataque(jogador);
-        }
-            return foiAtacado;
-
-    }
-
-    public static boolean spawnarSobrevivente(Personagem jogador) {
-        boolean foiAtacado = false;
-        foiAtacado = Utilitario.getBooleanAleatorio();
-        if (foiAtacado) {
-            Criatura criatura = new Sobrevivente("Sobrevivente Perdido", 100.0, 15.0, 15.0);
-            criatura.ataque(jogador);
-        }
-        return foiAtacado;
-
-    }
-
-    public static boolean armadilhaInstaurada(Personagem jogador) {
-        boolean armadilhaInstaurada = false;
-        armadilhaInstaurada = Utilitario.getBooleanAleatorio();
-        if (armadilhaInstaurada) {
-            jogador.diminuirVida(25.0);
-            jogador.diminuirEnergia(15.0);
-            jogador.diminuirSanidade(22.0);
-        }
-        return armadilhaInstaurada;
-    }
 }
